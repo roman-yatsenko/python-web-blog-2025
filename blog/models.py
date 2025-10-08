92ead490416cd67cb67897cfd81434b28e9ae577
YaRo <roman.yatsenko@hneu.net>
2025-10-08 21:00:23 +0300

    Add get_absolute_url method to Post model and update list template for URL resolution

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 class PublishedManager(models.Manager):
@@ -32,3 +33,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
+    
