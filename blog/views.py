0a2f6953f9cc7709c1478c97467c025755d6ef1e
YaRo <roman.yatsenko@hneu.net>
2025-10-15 20:25:53 +0300

    Refactor blog post listing to use class-based view and update pagination template reference

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
+
+class PostListView(ListView):
+    """Альтернативне подання списку постів"""
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     post_list = Post.published.all()
