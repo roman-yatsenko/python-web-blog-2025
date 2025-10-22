80223bee303ebdb60ade12147d7338a377a902b0
YaRo <roman.yatsenko@hneu.net>
2025-10-22 21:03:54 +0300

    Add comment functionality with form handling and template integration

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 class PostListView(ListView):
     """Альтернативне подання списку постів"""
@@ -58,3 +59,26 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # Коментар було відправлено
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Створити об'єкт Comment
+        comment = form.save(commit=False)
+        # Призначити пост коментарю
+        comment.post = post
+        # Зберегти коментар в БД
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        context={
+            'post': post,
+            'form': form,
+            'comment': comment,
+        }
+    )
