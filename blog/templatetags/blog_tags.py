473c543fe620409a53fbd5153905c9e6bfd907fd
YaRo <roman.yatsenko@hneu.net>
2025-11-12 20:53:36 +0200

    Implement markdown support for post body rendering

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 from ..models import Post
 
@@ -22,3 +24,8 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).filter(total_comments__gte=1).order_by('-total_comments', '-publish')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
